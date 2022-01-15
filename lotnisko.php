950b72295674dc093d70d656ffa4bbf6393ef3c9
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2022-01-15 13:14:46 +0100

    Add files via upload

--- lotnisko.php
+++ lotnisko.php
@@ -4,14 +4,14 @@
   <head>
 
      <meta charset="utf-8">
-     <title>Moja witryna</title>
+     <title>Port Lotniczy</title>
 
      <meta name="description" content="Opis zawartości strony dla wyszukiwarek">
      <meta name="keywords" content="słowa, kluczowe, opisujące, zawartość">
      <meta name="author" content="Jan Programista">
 
      <meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
-     <link rel="stylesheet" href="arkusz.css">
+     <link rel="stylesheet" href="styl5.css">
      <script src="skrypt.js"></script>
 
   </head>
