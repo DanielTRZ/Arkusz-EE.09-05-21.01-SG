1fb3cc789097d1c39e36cf0980904a9d7443805b
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2022-01-15 14:56:56 +0100

    Add files via upload

--- lotnisko.php
+++ lotnisko.php
@@ -5,38 +5,40 @@
 
      <meta charset="utf-8">
      <title>Port Lotniczy</title>
-
      <meta name="description" content="Opis zawartości strony dla wyszukiwarek">
      <meta name="keywords" content="słowa, kluczowe, opisujące, zawartość">
      <meta name="author" content="Jan Programista">
-
-     <meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
      <link rel="stylesheet" href="styl5.css">
-     <script src="skrypt.js"></script>
 
   </head>
+  
   <body>
+  
   <div id="baner1">
       <img src="zad5.png" alt="logo lotnisko">
   </div>
+  
   <div id="baner2">
       <h1>Przyloty</h1>
   </div>
+  
   <div id="baner3">
   <h3>Przydatne linki</h3>
   <a href="Kwerendy.txt" target="_blank">Pobierz...</a>
-
   </div>
+  
   <div id="glowny">
    
     
   </div>
+  
   <div id="stopka1">
       
   </div>
+  
   <div id="stopka2">
-      <a>Autor: </a>
+      <p>Autor:0000000000</p>
   </div>
+  
   </body>
-
 </html>
